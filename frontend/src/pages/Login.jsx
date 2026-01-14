1385cb1f629df7468a20a9d91460c939e665aea6
zzh <3340730267@qq.com>
2026-01-14 21:36:16 +0800

    family-education

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,11 +9,11 @@ export default function Login() {
       <form className='auth-form'>
         <div className="auth-form__group">
           <i className='iconfont icon-zhanghao'></i>
-          <input type="tel" placeholder='请输入手机号或邮箱' className='auth-form__input'/>
+          <input type="tel" placeholder='请输入手机号或邮箱' className='auth-form__input' />
         </div>
         <div className="auth-form__group">
           <i className='iconfont icon-mima-copy'></i>
-          <input type="password" placeholder='请输入密码' className='auth-form__input'/>
+          <input type="password" placeholder='请输入密码' className='auth-form__input' />
         </div>
         <div className="auth-form__forgot-wrapper">
           <a href="#" className='auth-form__forgot'>忘记密码?</a>
@@ -22,7 +22,5 @@ export default function Login() {
       </form>
     </div>
   )
-  
 }
 
- 
