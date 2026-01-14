8c4704679ab40dde07f300b1f9635bfa03746be7
zzh <3340730267@qq.com>
2026-01-14 21:44:47 +0800

    test

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,14 +7,17 @@ export default function Login() {
   return (
     <div>
       <form className='auth-form'>
+        
         <div className="auth-form__group">
           <i className='iconfont icon-zhanghao'></i>
           <input type="tel" placeholder='请输入手机号或邮箱' className='auth-form__input' />
         </div>
+
         <div className="auth-form__group">
           <i className='iconfont icon-mima-copy'></i>
           <input type="password" placeholder='请输入密码' className='auth-form__input' />
         </div>
+
         <div className="auth-form__forgot-wrapper">
           <a href="#" className='auth-form__forgot'>忘记密码?</a>
         </div>
