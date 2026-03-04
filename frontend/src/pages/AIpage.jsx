08a5257734b7a7e8f11380cbcbd9c580967b1782
zzh <3340730267@qq.com>
2026-03-04 14:01:18 +0800

    item

--- frontend/src/pages/AIpage.jsx
+++ frontend/src/pages/AIpage.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function AIpage() {
   return (
-    <div>AIpage</div>
+    <div className='ai-page-root'>
+      <header className='ai-page-header'>
+         <h1>AI小伙伴</h1>
+         <p>让ai陪伴孩子成长</p>
+      </header>
+
+      <section className='ai-page-content'>
+        <div className='ai-feature-card'>
+          <i className='iconfont'></i>
+          <h3>智能对话</h3>
+          <p>与ai进行智能对话,解答各种问题</p>
+        </div>
+      </section>
+    </div>
   )
 }
