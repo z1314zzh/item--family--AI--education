f6e73a0f716ce4f25881b45c010f829e6da73da5
zzh <3340730267@qq.com>
2026-03-08 18:43:24 +0800

    item

--- frontend/src/pages/AccountSetting.jsx
+++ frontend/src/pages/AccountSetting.jsx
@@ -1,23 +1,42 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { List } from 'antd-mobile'
-import { Avatar } from 'antd-mobile'
-import { useEffect , useState } from 'react'
+import { Avatar, ActionSheet, List, ImageViewer } from 'antd-mobile'
+import { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
+import '../styles/AccountSetting.less'
 
 export default function AccountSetting() {
   const [account, setAccount] = useState('')
   const [avatar, setAvatar] = useState('')
   const [nickname, setNickname] = useState('')
   const navigate = useNavigate()
-  
+  const [visible, setVisible] = useState(false)
+  const [imageVisible, setImageVisible] = useState(false)
+  const avatarRef = useRef(null)
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [uploading, setUploading] = useState(false)
+  const actions = [
+    { text: '从相册中选择', key: 'select' }
+  ]
+
   useEffect(() => {
     axios.get('/api/auth/info').then(res => {
-        setAccount(res.data.account)
-        setAvatar(res.data.avatar)
-        setNickname(res.data.nickname)
+      setAccount(res.data.account)
+      setAvatar(res.data.avatar)
+      setNickname(res.data.nickname)
     })
   }, [])
+
+  const avatarChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      const imageUrl = URL.createObjectURL(file) //将 file 对象转换成 url
+      setUploading(imageUrl)
+      setImageVisible(true)
+      // 通过后端向数据库上传头像
+    }
+
+  }
 
 
   return (
@@ -37,12 +56,48 @@ export default function AccountSetting() {
 
       <section className="account-setting__section">
         <List>
-          <List.Item onClick={() => {}} extra={<Avatar style={{'--border-radius': '50%'}} src='' size={40}  />}>头像</List.Item>
-          <List.Item onClick={() => {}} extra='zzh' clickable>昵称</List.Item>
-          <List.Item onClick={() => {}} extra='17879987232' clickable>手机号</List.Item>
-          <List.Item onClick={() => {}} extra='修改密码'>密码</List.Item>
+          <List.Item onClick={() => { setVisible(true) }} extra={<Avatar style={{ '--border-radius': '50%' }} src={selectedImage} size={40} />} clickable>头像</List.Item>
+          <List.Item onClick={() => { }} extra='zzh' clickable>昵称</List.Item>
+          <List.Item onClick={() => { }} extra='17879987232' clickable>手机号</List.Item>
+          <List.Item onClick={() => { }} extra='修改密码'>密码</List.Item>
         </List>
       </section>
+
+      <input type="file" accept='image/*' ref={avatarRef} onChange={avatarChange} style={{ display: 'none' }} />
+
+      <ActionSheet
+        visible={visible}
+        actions={actions}
+        onClose={() => setVisible(false)}
+        cancelText='手滑了'
+        onAction={(action, index) => {
+          //从相册中选择
+          if (action.key === 'select') {
+            setVisible(true)
+            avatarRef.current.click()
+          }
+          setVisible(false)
+        }}
+      />
+      <ImageViewer
+        classNames={{
+          mask: 'customize-mask',
+          body: 'customize-body',
+        }}
+        image={uploading}
+        visible={imageVisible}
+        renderFooter={(uploading) => (
+          <div className='footer'>
+            <div className='footerButton' onClick={() => {
+              setImageVisible(false)
+              setSelectedImage(uploading)
+            }}>确认上传</div>
+          </div>
+        )}
+        onClose={() => {
+          setImageVisible(false)
+        }}
+      />
 
 
 
